f49899e988ebd9afcbfd3f7ac0cfb3653780edf4
Jim Garrison <jim@garrison.cc>
2017-10-06 16:30:54 -0400

    Add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,20 @@
 # This file is a part of ElasticArrays.jl, licensed under the MIT License (MIT).
 
+using ElasticArrays
+using Base.Test
+
 @Base.Test.testset "Package ElasticArrays" begin
+    dims = (3, 2)
+    N = 10
+    a = ElasticArray{Float64}(dims..., 0)
+    mats = Array{Float64,length(dims)}[]
+    for i in 1:N
+        mat = rand(dims...)
+        append!(a, mat)
+        push!(mats, mat)
+    end
+    @test size(a) == (dims..., N)
+    for i in 1:N
+        @test @view(a[:,:,i]) == mats[i]
+    end
 end
